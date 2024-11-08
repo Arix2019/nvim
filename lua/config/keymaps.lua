e364607d55677d2d856c9d8d050121da41927308
Ariel B <arix0001@gmail.com>
2024-11-08 14:29:56 -0300

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = LazyVim.safe_keymap_set
-map("n", "<leader>t", ":ToggleTerm direction=float <cr>", { desc = "open terminal" })
-map("n", "<leader>T", ":ToggleTerm direction=horizontal <cr>", { desc = "open terminal" })
+map("n", "<leader>q", ":ToggleTerm direction=float <cr>", { desc = "open terminal" })
+map("n", "<leader>w", ":ToggleTerm direction=horizontal <cr>", { desc = "open terminal" })
 map("n", "<C-n>", ":Neotree toggle<cr>", {})
 map("n", "<C-l>", ":Neotree last<cr>", {})
--- lua/plugins/vim-options.lua
+++ lua/plugins/vim-options.lua
@@ -1,3 +1,12 @@
+vim.cmd("set number")
+vim.cmd("set encoding=utf-8")
+vim.cmd("set cursorline")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
 vim.cmd("set shell=powershell")
 vim.cmd("set shellcmdflag=-command")
 vim.cmd("set shellquote='")
