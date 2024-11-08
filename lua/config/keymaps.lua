4aba396daf7dea23ad01ce222c9c800028e629aa
Ariel B <arix0001@gmail.com>
2024-11-08 13:48:26 -0300

    lazyvim/nvim for windows

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+map("n", "<leader>t", ":ToggleTerm direction=float <cr>", { desc = "open terminal" })
+map("n", "<leader>T", ":ToggleTerm direction=horizontal <cr>", { desc = "open terminal" })
+map("n", "<C-n>", ":Neotree toggle<cr>", {})
+map("n", "<C-l>", ":Neotree last<cr>", {})
---
+++ lua/plugins/vim-options.lua
@@ -0,0 +1,4 @@
+vim.cmd("set shell=powershell")
+vim.cmd("set shellcmdflag=-command")
+vim.cmd("set shellquote='")
+vim.cmd("set shellxquote=")
